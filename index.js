59d5c5e18e9ef413f2be2b9209c3949da036ec64
Jenn Leishman <jennifermleishman@gmail.com>
2025-12-08 10:03:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("listening at port 3000");
-});
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("listening at port" + PORT);
+    }
+);
